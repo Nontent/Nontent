c48ae1d0b13f20dd18ddf40965940f696c1654e0
Samuel GALIERE <samuel.galiere@epitech.eu>
2022-07-02 17:06:18 +0200

    [WIP]: working on login and register

---
+++ web-app/pages/connection.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<v-carousel v-model="model">
+			<v-carousel-item
+				v-for="(item, index) in connectionItems" :key="index"
+			>
+				<v-form>
+					
+				</v-form>	
+			</v-carousel-item>
+		<v-carousel>		
+	</div>
+</template>
+<script>
+export default {
+	name: 'connection',
+	data() {
+		return {
+			model: 0,
+			connectionItems: [
+				{ nameBtn: 'Connection', title: 'Welcome back!' },
+				{ namebtn: 'Register', title: 'Register now!' },
+			],
+		}
+	},
+}
+</script>
+<style>
+</style>
