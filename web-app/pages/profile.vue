e1c9b5c6395fd13eaf240e4d33fff491ef0d20b5
Samuel GALIERE <ho.sgaliere@cma-cgm.com>
2022-07-25 17:26:40 +0200

    [WIP]: starting work on profile page

---
+++ web-app/pages/profile.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<span class="text--h2">
+			<v-icon>mdi-account</v-icon>
+			{{ $store.state.user.username }}
+		</span>
+	</div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+</style>
