d20234d9560b35cf1a5b41554094167c053f3204
Samuel GALIERE <samuel.galiere@epitech.eu>
2022-11-29 12:35:02 +0100

    [ADD]: navbar style v0.1 + blank pages

--- web-app/components/NavBar.vue
+++ web-app/components/NavBar.vue
@@ -1,61 +1,66 @@
 <template>
-    <div class="grid grid-cols-3 gap-1 box-border h-16 w-128 p-4 border-4 mx-2 my-2 border-black">
-        <span class="justify-center font-bold">
-            nontent.
-        </span>
-        <div class="space-x-8 place-self-center">
-            <div class="inline" v-for="(link) in links" :key="link.name">
-                <nuxt-link :to="link.link">
-                    {{ link.name }}
-                </nuxt-link>
-            </div>
-        </div>
-        <div class="space-x-8 place-self-end">
-            <div class="bg-amber-400 align-top">
-                <CogIcon class="h-8 w-8 text-white"/>
-            </div>
-        </div>
-
-</div>
+	<div class="flex h-16 w-auto p-4 border-4 mx-2 my-2 border-black">
+		<span class="flex-none justify-center font-bold"> nontent. </span>
+		<span class="grow"></span>
+		<div class="grow-0 space-x-4">
+			<div class="inline" v-for="link in links" :key="link.name">
+				<nuxt-link :to="link.link">
+					{{ link.name }}
+				</nuxt-link>
+			</div>
+		</div>
+		<span class="grow"></span>
+		<div class="self-center flex space-x-2">
+			<div class="bg-amber-400 h-10 w-10 btn">
+				<CogIcon class="self-center h-8 w-8 text-white" />
+			</div>
+			<div class="bg-gray-700 h-10 w-10 btn">
+				<ArrowLeftOnRectangleIcon
+					class="h-8 w-8 self-center text-white"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { CogIcon } from '@heroicons/vue/24/solid'
+import { CogIcon } from "@heroicons/vue/24/solid";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/vue/24/solid";
+
 export default {
-    data() {
-        return {
-            links: [
-                {
-                    name: 'home',
-                    link: '/'
-                },
-                {
-                    name: 'social reports',
-                    link: '/social-reports'
-                },
-                {
-                    name: 'profile',
-                    link: '/profile'
-                }
-            ],
-            listButtons: [
-                {
-                    name: 'settings',
-                    link: '/settings'
-                },
-                {
-                    name: 'sign out',
-                    link: '/sign-out'
-                }
-            ]
-        }
-    },
-    components: {
-        CogIcon
-    }
-}
+	data() {
+		return {
+			links: [
+				{
+					name: "home",
+					link: "/",
+				},
+				{
+					name: "social reports",
+					link: "/social-reports",
+				},
+				{
+					name: "profile",
+					link: "/profile",
+				},
+			],
+			listButtons: [
+				{
+					name: "settings",
+					link: "/settings",
+				},
+				{
+					name: "sign out",
+					link: "/sign-out",
+				},
+			],
+		};
+	},
+	components: {
+		CogIcon,
+		ArrowLeftOnRectangleIcon,
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ web-app/pages/social-reports.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>Social Reports</div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
