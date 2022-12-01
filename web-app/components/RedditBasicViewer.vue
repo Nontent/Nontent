96ee2260f48dfd88eb1386a1323dcae32abcaf03
Samuel GALIERE <samuel.galiere@epitech.eu>
2022-12-01 15:29:42 +0100

    [WIP]: added two temporary Social Media components

--- web-app/components/RedditBasicViewer.vue
+++ web-app/components/RedditBasicViewer.vue
@@ -1,21 +1,22 @@
 <template>
-	<div class="main-box grid grid-cols-2 grid-rows-2 justify-items-center">
-		<div class="col-span-2">
+	<div class="main-box grid grid-cols-3 grid-rows-2 justify-items-center">
+		<div class="col-span-3">
 			<Icon
-				name="mingcute:twitter-line"
+				name="mingcute:reddit-line"
 				class="h-10 w-10"
-				color="#1DA1F2"
+				color="#FF4500"
 			/>
 			-
 			{{ userName }}
 		</div>
 		<div>{{ followers }} followers(s)</div>
-		<div>{{ subs }} subsription(s)</div>
+		<div>{{ subs }} subscribtion(s)</div>
+		<div>{{ subReddits.length }} sub-reddit(s) followed</div>
 	</div>
 </template>
 <script>
 export default {
-	name: "TwitterBasicViewer",
+	name: "RedditBasicViewer",
 	props: {
 		userName: {
 			type: String,
@@ -28,6 +29,10 @@ export default {
 		subs: {
 			type: Number,
 			default: 0,
+		},
+		subReddits: {
+			type: Array,
+			default: [],
 		},
 	},
 };
--- web-app/pages/social-reports.vue
+++ web-app/pages/social-reports.vue
@@ -1,9 +1,37 @@
 <template>
-	<TwitterBasicViewer />
+	<div>
+		<TwitterBasicViewer
+			:userName="fakeUser.name"
+			:followers="fakeUser.followers"
+			:subs="fakeUser.subs"
+		/>
+		<RedditBasicViewer
+			:userName="fakeUser.name"
+			:followers="fakeUser.followers"
+			:subs="fakeUser.subs"
+			:subReddits="fakeUser.subReddits"
+		/>
+	</div>
 </template>
 <script>
 export default {
 	name: "SocialReports",
+	data() {
+		return {
+			fakeUser: {
+				name: "@fakeNontentUser",
+				followers: 110,
+				subs: 1200,
+				subReddits: [
+					"r/nontent",
+					"r/nontent",
+					"r/nontent",
+					"r/nontent",
+					"r/nontent",
+				],
+			},
+		};
+	},
 };
 </script>
 <style></style>
