eb6b5a61a3f5a917453a11eac9d02c39b7a609e6
GuiEpi <guillaume.coussot@epitech.eu>
2023-04-06 13:27:46 +0200

    add progress bar in dataset maker

--- ai/ai/dataset_maker.py
+++ ai/ai/dataset_maker.py
@@ -1,4 +1,4 @@
-import csv
+from rich.progress import Progress, SpinnerColumn, TextColumn
 from ai.data import (
     war_tweet,
     gaming_tweet,
@@ -13,34 +13,43 @@ from ai.data import (
     marketing_tweet,
     science_tweet,
 )
+import csv
+
 
 
 def main() -> None:
-    with open("../data/tweets.csv", "w", newline="", encoding="utf-8") as f:
-        fieldnames = ["tweet", "label"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-        for tweet in war_tweet:
-            writer.writerow({"tweet": tweet, "label": "WAR"})
-        for tweet in gaming_tweet:
-            writer.writerow({"tweet": tweet, "label": "GAMING"})
-        for tweet in tech_tweet:
-            writer.writerow({"tweet": tweet, "label": "TECH"})
-        for tweet in science_tweet:
-            writer.writerow({"tweet": tweet, "label": "SCIENCE"})
-        for tweet in music_tweet:
-            writer.writerow({"tweet": tweet, "label": "MUSIC"})
-        for tweet in policy_tweet:
-            writer.writerow({"tweet": tweet, "label": "POLICY"})
-        for tweet in cinema_tweet:
-            writer.writerow({"tweet": tweet, "label": "CINEMA"})
-        for tweet in sport_tweet:
-            writer.writerow({"tweet": tweet, "label": "SPORT"})
-        for tweet in medical_tweet:
-            writer.writerow({"tweet": tweet, "label": "MEDICAL"})
-        for tweet in literature_tweet:
-            writer.writerow({"tweet": tweet, "label": "LITERATURE"})
-        for tweet in food_tweet:
-            writer.writerow({"tweet": tweet, "label": "FOOD"})
-        for tweet in marketing_tweet:
-            writer.writerow({"tweet": tweet, "label": "MARKETING"})
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Making dataset...", total=None)
+        with open("../data/tweets.csv", "w", newline="", encoding="utf-8") as f:
+            fieldnames = ["tweet", "label"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            for tweet in war_tweet:
+                writer.writerow({"tweet": tweet, "label": "WAR"})
+            for tweet in gaming_tweet:
+                writer.writerow({"tweet": tweet, "label": "GAMING"})
+            for tweet in tech_tweet:
+                writer.writerow({"tweet": tweet, "label": "TECH"})
+            for tweet in science_tweet:
+                writer.writerow({"tweet": tweet, "label": "SCIENCE"})
+            for tweet in music_tweet:
+                writer.writerow({"tweet": tweet, "label": "MUSIC"})
+            for tweet in policy_tweet:
+                writer.writerow({"tweet": tweet, "label": "POLICY"})
+            for tweet in cinema_tweet:
+                writer.writerow({"tweet": tweet, "label": "CINEMA"})
+            for tweet in sport_tweet:
+                writer.writerow({"tweet": tweet, "label": "SPORT"})
+            for tweet in medical_tweet:
+                writer.writerow({"tweet": tweet, "label": "MEDICAL"})
+            for tweet in literature_tweet:
+                writer.writerow({"tweet": tweet, "label": "LITERATURE"})
+            for tweet in food_tweet:
+                writer.writerow({"tweet": tweet, "label": "FOOD"})
+            for tweet in marketing_tweet:
+                writer.writerow({"tweet": tweet, "label": "MARKETING"})
+    print("Done!")
