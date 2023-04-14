981b4e464d25c709db40da73f7198432a396a3b2
unknown <nicolas1.blanc@epitech.eu>
2023-04-14 10:29:15 +0200

    [FIX]: user password now correctly hash on update

--- backend/src/routes/user/user.service.js
+++ backend/src/routes/user/user.service.js
@@ -31,6 +31,9 @@ exports.userCreationService = async (userBody) => {
 }
 
 exports.userUpdateService = async (userId, userBody) => {
+    if (userBody.password) {
+        userBody.password = md5(userBody.password)
+    }
     console.log('props', userId, userBody)
     if (!userId || !userBody) {
         throw new Error('Props invalid');
