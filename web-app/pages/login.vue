e6d4a4a853f3e16d00936c769b1741e8ed8ad454
Samuel GALIERE <samuel.galiere@epitech.eu>
2023-04-15 16:18:56 +0200

    [REFACTO]: enhanced login page behaviour - error handling + security

--- web-app/pages/login.vue
+++ web-app/pages/login.vue
@@ -4,22 +4,59 @@
 			<h1 class="flex justify-center font-bold mt-11 text-5xl mb-11">
 				nontent.
 			</h1>
-			<div class="input-form-box mb-11">
-				<input
-					id="email_login"
-					type="text"
-					class="outline-none w-full"
-					placeholder="EMAIL"
-				/>
+			<div class="mb-11">
+				<div class="input-form-box">
+					<input
+						id="email_login"
+						type="text"
+						class="outline-none w-full"
+						placeholder="email."
+						v-model="loginEmail"
+					/>
+				</div>
+				<div
+					v-if="!isEmailValid"
+					class="mt-1 text-sm text-red-500 container flex mx-auto"
+				>
+					Please enter a valid email address (example@domain.com).
+				</div>
 			</div>
-			<div class="input-form-box">
+			<div class="input-form-box flex">
 				<input
 					id="password_login"
-					type="text"
+					:type="showPassword ? 'text' : 'password'"
 					class="outline-none w-full"
-					placeholder="PASSWORD"
+					placeholder="password."
+					v-model="loginPassword"
 				/>
+				<div
+					class="flex items-center pr-2 ml-auto"
+					@click="showPassword = !showPassword"
+				>
+					<Icon
+						:name="
+							showPassword
+								? 'ant-design:eye-invisible-filled'
+								: 'ant-design:eye-filled'
+						"
+						class="w-6 h-6"
+						color="#FFB100"
+					/>
+				</div>
 			</div>
+			<div
+				v-if="!isPasswordValid"
+				class="mt-1 text-sm text-red-500 container flex mx-auto"
+			>
+				Please enter a valid password (min. 6 characters)
+			</div>
+			<div
+				v-if="loginError"
+				class="mt-1 text-sm text-red-500 container flex mx-auto"
+			>
+				Wrong email or password. Please try again.
+			</div>
+
 			<div class="flex justify-center">
 				<div class="group mt-11">
 					<button
@@ -62,14 +99,57 @@ export default {
 		const store = useMainStore();
 		return { store };
 	},
+	data() {
+		return {
+			showPassword: false,
+			loginEmail: "",
+			loginPassword: "",
+			isEmailValid: true,
+			isPasswordValid: true,
+			loginError: false,
+		};
+	},
 	methods: {
 		toRegister() {
 			this.$router.push("/register");
 		},
+		verifyLogin() {
+			if (this.loginEmail == "" || this.loginPassword == "") {
+				this.isEmailValid = false;
+				this.isPasswordValid = false;
+				return false;
+			} else {
+				this.isEmailValid = true;
+				this.isPasswordValid = true;
+			}
+			if (
+				!this.loginEmail.includes("@") ||
+				!this.loginEmail.includes(".")
+			) {
+				this.isEmailValid = false;
+				return false;
+			} else {
+				this.isEmailValid = true;
+			}
+			if (this.loginPassword.length < 6) {
+				this.isPasswordValid = false;
+				return false;
+			} else {
+				this.isPasswordValid = true;
+			}
+			this.isEmailValid = true;
+			this.isPasswordValid = true;
+			return true;
+		},
 		async login() {
-			const email = document.getElementById("email_login").value;
-			const password = document.getElementById("password_login").value;
-			const user = { email: email, password: password };
+			const isValid = this.verifyLogin();
+			if (!isValid) {
+				return;
+			}
+			const user = {
+				email: this.loginEmail,
+				password: this.loginPassword,
+			};
 			try {
 				const response = await Providers.login(user);
 				if (response.status === 200) {
@@ -87,17 +167,18 @@ export default {
 					this.store.setEmail(userResponse.data.email);
 					this.store.increment();
 					if (userResponse.data.socialNetworks.length > 0) {
-						userResponse.data.socialNetworks.forEach((account) => {
-							this.store.addAccount({
-								provider: account.provider,
-								platformUsername: userResponse.data.username,
-							});
+						this.store.increment();
+						this.store.addAccount({
+							provider: "Twitter",
+							username: userResponse.data.twitterUsername
+								? userResponse.data.twitterUsername
+								: "",
 						});
 					}
 					this.$router.push("/");
 				}
 			} catch (error) {
-				console.error(error);
+				this.loginError = true;
 			}
 		},
 	},
